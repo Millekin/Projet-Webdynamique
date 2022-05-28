801f2dc000415c43f03b6d3f94b9d36666dc07f9
Millekin <41106602+Millekin@users.noreply.github.com>
2022-05-28 11:10:11 +0200

    Update and rename votre_compte.html to votre_compte.php

--- votre_compte.php
+++ votre_compte.php
@@ -45,18 +45,18 @@
                 <div class="col-12">
                     <nav class="main-nav">
                         <!-- CHARGEMENT LOGO -->
-                        <a href="index.html" class="logo">
+                        <a href="Accueil.php" class="logo">
                             VOTRE COMPTE
                         </a>
                         <!-- FIN LOGO -->
 
                         <!-- MENU -->
                         <ul class="nav">
-                            <li class="scroll-to-section"><a href="Accueil.html">Accueil</a></li>
-                            <li class="scroll-to-section"><a href="tout_parcourir.html">TOUT PARCOURIR</a></li>
-                            <li class="scroll-to-section"><a href="rendez-vous.html">RENDEZ-VOUS</a></li>
+                            <li class="scroll-to-section"><a href="Accueil.php">Accueil</a></li>
+                            <li class="scroll-to-section"><a href="tout_parcourir.php">TOUT PARCOURIR</a></li>
+                            <li class="scroll-to-section"><a href="rendez-vous.php">RENDEZ-VOUS</a></li>
                             <li class="submenu">
-                                <a href="votre_compte.html" class="active">Votre Compte</a>
+                                <a href="votre_compte.php" class="active">Votre Compte</a>
                                 <ul>
                                     <li><a href="">Connexion</a></li>
                                     <li><a href="">Fonctionnalités</a></li>
@@ -64,7 +64,7 @@
                                     <li><a href="">Deconnexion</a></li>
                                 </ul>
                             </li>
-                            <li class="scroll-to-section"><a href="contactez-nous.html">Contactez-nous</a></li> 
+                            <li class="scroll-to-section"><a href="contactez-nous.php">Contactez-nous</a></li> 
                             <div class="search-icon">
                                 <a href="#search"><i class="fa fa-search"></i></a>
                             </div>
